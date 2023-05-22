7dbc8f9004320972904a43f87ebe3443d6adb753
arsen andjelic <arsenandjelic78@gmail.com>
2023-05-22 11:10:51 +0200

    peti zadatak

--- zoo/src/App.js
+++ zoo/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
         <AnimalList />
+        
         <p></p>
         <a
           className="App-link"
@@ -20,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
--- zoo/src/components/AnimalList.jsx
+++ zoo/src/components/AnimalList.jsx
@@ -26,6 +26,7 @@ const listOfAnimals = [
   },
 ];
 
+
 const AnimalList = () => {
   //IDE HOOK
   const [animals, setAnimals] = useState(listOfAnimals);
@@ -43,8 +44,69 @@ const AnimalList = () => {
       ...prevState.filter((_, id) => id !== index),
     ]);
   };
+  
+  const [name, setName] = useState('');
+  const [species, setSpecies] = useState('');
+  const [date_of_birth, setDateOfBirth] = useState('');
+  const [message, setMessage] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault(); // Prevents default behaviour of form submit action
+
+//  };
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  setMessage(`Hello ${name}!`);
+  setName('');
+  setSpecies('');
+  setDateOfBirth('');
+};
+
   return (
     <div>
+      <form>
+        <label onSubmit={handleSubmit}>
+          Name:
+          <input
+            type='text'
+            id="name"
+            name="animalName"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          </label>
+          <label>
+          Species:
+          <input
+            type='text'
+            id="species"
+            name="animalSpecies"
+            value={species}
+            onChange={(e) => {
+              setSpecies(e.target.value);
+            }}
+          />
+          </label>
+          <label>
+          Date of Birth:
+          <input
+            type='text'
+            id="date_of_birth"
+            value={date_of_birth}
+            onChange={(e) => {
+              setDateOfBirth(e.target.value);
+            }}
+          />
+          </label>
+          <button type="submit">Submit</button>
+          <h2>{message}</h2>
+
+
+      </form>
       <table>
         <thead>
           <tr>
@@ -66,10 +128,19 @@ const AnimalList = () => {
                 <button onClick={() => MoveToTop(index)}>Move to top</button>
               </td>
             </tr>
+            
           ))}
         </tbody>
       </table>
     </div>
+    
   );
 };
+
+
+
+
+
+
+
 export default AnimalList;
